757ae4137a909a5126729d875436edaf0ad1f9b0
niles87 <nbing19@gmail.com>
2020-02-13 20:25:12 -0800

    changed underscore to tilde for readablility refactored display letter function

--- letter.js
+++ letter.js
@@ -1,19 +1,19 @@
-function Letter(character) {
+var Letter = function(character) {
   this.character = character;
   this.bool = false;
   this.displayLetter = function() {
-    if (this.bool) {
+    if (this.bool === true) {
       return this.character;
     } else {
-      return "_";
+      return "~";
     }
   };
-  this.guess = function(char) {
-    if (char === this.character) {
+  this.userGuess = function(char) {
+    if (char == this.character) {
       this.bool = true;
     }
   };
-}
+};
 
 // Module needed for word.js
 module.exports = Letter;
